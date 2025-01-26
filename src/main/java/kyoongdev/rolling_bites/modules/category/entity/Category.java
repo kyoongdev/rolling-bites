3b017e7ff3dc0321441ce0bdb44021389d18ad2b
kyoongdev <9898junjun2@gmail.com>
2025-01-26 15:50:27 +0900

    feat(category): category repository and test

--- src/main/java/kyoongdev/rolling_bites/modules/category/entity/Category.java
+++ src/main/java/kyoongdev/rolling_bites/modules/category/entity/Category.java
@@ -6,12 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "category")
 @Entity
 public class Category {
---
+++ src/test/java/kyoongdev/rolling_bites/modules/category/CategoryRepositoryTest.java
@@ -0,0 +1,37 @@
+package kyoongdev.rolling_bites.modules.category;
+
+
+import java.util.Optional;
+import kyoongdev.rolling_bites.modules.category.entity.Category;
+import kyoongdev.rolling_bites.modules.category.repository.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CategoryRepositoryTest {
+
+  @Autowired
+  CategoryRepository categoryRepository;
+
+
+  @Test
+  @DisplayName("카테고리 생성 및 조회")
+  void createCategory() {
+    Category newCategory = Category.builder().name("분식").build();
+
+    categoryRepository.save(newCategory);
+
+    Optional<Category> category = categoryRepository.findCategoryByName("분식");
+
+    Assertions.assertTrue(category.isPresent());
+
+    Category realCategory = category.get();
+
+    Assertions.assertEquals(realCategory.getName(), "분식");
+  }
+}
