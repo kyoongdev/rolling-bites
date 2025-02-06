8baf3e73804f5bc7dc2578f8e48f4e8f76e6d136
kyoongdev <9898junjun2@gmail.com>
2025-02-06 14:29:26 +0900

    feat(api-client): append uri component builder

--- src/main/java/kyoongdev/rolling_bites/common/apiClient/ApiClientAdapter.java
+++ src/main/java/kyoongdev/rolling_bites/common/apiClient/ApiClientAdapter.java
@@ -6,6 +6,6 @@ import java.util.Map;
 //TODO: Post, Put, Patch, Delete 추가
 public interface ApiClientAdapter {
 
-  Map get(Map<String, String> headers, Map<String, String> queryParams, String url);
+  Map get(String url, Map<String, String> headers, Map<String, String> queryParams);
 
 }
--- src/main/java/kyoongdev/rolling_bites/common/apiClient/WebClientAdapter.java
+++ src/main/java/kyoongdev/rolling_bites/common/apiClient/WebClientAdapter.java
@@ -1,7 +1,9 @@
 package kyoongdev.rolling_bites.common.apiClient;
 
+import java.net.URI;
 import java.util.Map;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class WebClientAdapter implements ApiClientAdapter {
 
@@ -12,10 +14,17 @@ public class WebClientAdapter implements ApiClientAdapter {
   }
 
   @Override
-  public Map get(Map<String, String> headers, Map<String, String> queryParams, String url) {
-    WebClient.RequestHeadersSpec<?> request = webClient.get().uri(url);
+  public Map get(String url, Map<String, String> headers, Map<String, String> queryParams) {
+    UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(url);
+    
+    if (queryParams != null) {
+      queryParams.forEach(uriBuilder::queryParam);
+    }
 
-    // 헤더 설정
+    URI urlResult = uriBuilder.build().toUri();
+
+    WebClient.RequestHeadersSpec<?> request = webClient.get().uri(urlResult);
+
     if (headers != null) {
       request.headers(httpHeaders -> headers.forEach(httpHeaders::set));
     }
